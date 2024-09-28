231feb114f53cceb95109ec00c7c95d3fc53c133
seoyoon04 <sykim8483@naver.com>
2024-09-28 17:42:26 +0900

    초기설정, 회원가입 페이지'

--- 2024bw/src/App.js
+++ 2024bw/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import Register from './pages/Register'
 import './App.css';
+
+const router = createBrowserRouter([
+  {
+    path :"/",
+    element: <Register/>
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RouterProvider router={router}/>
+  )
 }
+// function App() {
+//   return (
+//     <Register/>
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Edit <code>src/App.js</code> and save to reload.
+//     //     </p>
+//     //     <a
+//     //       className="App-link"
+//     //       href="https://reactjs.org"
+//     //       target="_blank"
+//     //       rel="noopener noreferrer"
+//     //     >
+//     //       Learn React
+//     //     </a>
+//     //   </header>
+//     // </div>
+//   );
+// }
 
 export default App;
